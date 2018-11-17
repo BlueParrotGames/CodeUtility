cf0267a691dd718f3f863038ccfae18f611d22cc
Grootloops33 <43176241+Grootloops33@users.noreply.github.com>
2018-11-17 19:53:40 +0100

    Added a file that detects tracked files.

--- CodeUtility_Console/LineCounter/Counter.cs
+++ CodeUtility_Console/LineCounter/Counter.cs
@@ -18,6 +18,7 @@ namespace BPS.LineCounter
             List<FileInfo> cssFiles = new List<FileInfo>();
 
             List<string> childPaths = new List<string>();
+
             childPaths.AddRange(Directory.GetFiles(directory, "*", SearchOption.AllDirectories).Where(s => s.EndsWith(".cs") || s.EndsWith(".php") || s.EndsWith(".js") || s.EndsWith(".html") || s.EndsWith(".css")));
 
             for (int i = 0; i < childPaths.Count; i++)
@@ -37,11 +38,25 @@ namespace BPS.LineCounter
 
                 switch (extension)
                 {
-                    case ".cs": csFiles.Add(new FileInfo(fileName, lineCount)); break;
-                    case ".php": phpFiles.Add(new FileInfo(fileName, lineCount)); break;
-                    case ".js": jsFiles.Add(new FileInfo(fileName, lineCount)); break;
-                    case ".html": htmlFiles.Add(new FileInfo(fileName, lineCount)); break;
-                    case ".css": cssFiles.Add(new FileInfo(fileName, lineCount)); break;
+                    case ".cs":
+                        csFiles.Add(new FileInfo(fileName, lineCount));
+                        break;
+
+                    case ".php":
+                        phpFiles.Add(new FileInfo(fileName, lineCount));
+                        break;
+
+                    case ".js":
+                        jsFiles.Add(new FileInfo(fileName, lineCount));
+                        break;
+
+                    case ".html":
+                        htmlFiles.Add(new FileInfo(fileName, lineCount));
+                        break;
+
+                    case ".css":
+                        cssFiles.Add(new FileInfo(fileName, lineCount));
+                        break;
                 }
             }
 
@@ -98,6 +113,106 @@ namespace BPS.LineCounter
                 totalLines += cssFiles[i].LineCount;
             }
             Console.WriteLine("\n Total css lines: " + totalLines);
+        }
+
+        public static void CountLines(string[] directories, bool TakeInChildDirectories)
+        {
+            List<FileInfo> csFiles = new List<FileInfo>();
+            List<FileInfo> phpFiles = new List<FileInfo>();
+            List<FileInfo> jsFiles = new List<FileInfo>();
+            List<FileInfo> htmlFiles = new List<FileInfo>();
+            List<FileInfo> cssFiles = new List<FileInfo>();
+
+            List<string> childPaths = new List<string>();
+
+            for (int i = 0; i < directories.Length; i++)
+            {
+                childPaths.AddRange(Directory.GetFiles(directories[i], "*", SearchOption.AllDirectories).Where(s => s.EndsWith(".cs") || s.EndsWith(".php") || s.EndsWith(".js") || s.EndsWith(".html") || s.EndsWith(".css")));
+            }
+
+            for (int i = 0; i < childPaths.Count; i++)
+            {
+                string fileName = Path.GetFileName(childPaths[i]);
+                string extension = Path.GetExtension(childPaths[i]);
+
+                int lineCount = 0;
+                using (StreamReader sr = new StreamReader(childPaths[i]))
+                {
+                    while (sr.Peek() >= 0)
+                    {
+                        lineCount++;
+                        sr.ReadLine();
+                    }
+                }
+
+                switch (extension)
+                {
+                    case ".cs": csFiles.Add(new FileInfo(fileName, lineCount)); break;
+                    case ".php": phpFiles.Add(new FileInfo(fileName, lineCount)); break;
+                    case ".js": jsFiles.Add(new FileInfo(fileName, lineCount)); break;
+                    case ".html": htmlFiles.Add(new FileInfo(fileName, lineCount)); break;
+                    case ".css": cssFiles.Add(new FileInfo(fileName, lineCount)); break;
+                }
+            }
+
+            //C# INFO
+            int csLines = 0;
+            Console.WriteLine(" -- C# FILES: --\n");
+            for (int i = 0; i < csFiles.Count; i++)
+            {
+                Console.WriteLine("> " + csFiles[i].ToString());
+                csLines += csFiles[i].LineCount;
+            }
+            Console.WriteLine("\n Total c# lines: " + csLines);
+            Console.WriteLine("\n\n");
+
+            //PHP INFO
+            int phpLines = 0;
+            Console.WriteLine("-- PHP FILES: --\n");
+            for (int i = 0; i < phpFiles.Count; i++)
+            {
+                Console.WriteLine("> " + phpFiles[i].ToString());
+                phpLines += phpFiles[i].LineCount;
+            }
+            Console.WriteLine("\n Total php lines: " + phpLines);
+            Console.WriteLine("\n\n");
+
+            //JAVASCRIPT INFO
+            int jsLines = 0;
+            Console.WriteLine("-- JAVASCRIPT FILES: --\n");
+            for (int i = 0; i < jsFiles.Count; i++)
+            {
+                Console.WriteLine("> " + jsFiles[i].ToString());
+                jsLines += jsFiles[i].LineCount;
+            }
+            Console.WriteLine("\n Total javascript lines: " + jsLines);
+            Console.WriteLine("\n\n");
+
+            //HTML INFO
+            int htmlLines = 0;
+            Console.WriteLine("-- HTML FILES: --\n");
+            for (int i = 0; i < htmlFiles.Count; i++)
+            {
+                Console.WriteLine(">" + htmlFiles[i].ToString());
+                htmlLines += htmlFiles[i].LineCount;
+            }
+            Console.WriteLine("\n Total html lines: " + htmlLines);
+            Console.WriteLine("\n\n");
+
+            //CSS INFO
+            int cssLines = 0;
+            Console.WriteLine("-- CSS FILES: --\n");
+            for (int i = 0; i < cssFiles.Count; i++)
+            {
+                Console.WriteLine(">" + cssFiles[i].ToString());
+                cssLines += cssFiles[i].LineCount;
+            }
+            Console.WriteLine("\n Total css lines: " + cssLines + "\n");
+
+            Console.WriteLine("------------------------");
+            int absoluteLines = csLines + phpLines + jsLines + htmlLines + cssLines;
+            Console.WriteLine("\n Absolute Total Lines: " + absoluteLines);
+
         }
 
         struct FileInfo
